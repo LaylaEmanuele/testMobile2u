c3712e7f7e1513626ce3b5f625818690abd8c1d9
Layla Emanuele <69046321+LaylaEmanuele@users.noreply.github.com>
2021-09-17 00:56:32 -0300

    Initial Commit

---
+++ testMobile2u/testMobile2uApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMobile2uApp.swift
+//  testMobile2u
+//
+//  Created by Layla Emanuele on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct testMobile2uApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
